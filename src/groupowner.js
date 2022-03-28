438856b65129f4479166b2864a9f746609cebbc5
ArulMC123 <99807639+ArulMC123@users.noreply.github.com>
2022-03-29 05:32:07 +0700

    Update groupowner.js

--- src/groupowner.js
+++ src/groupowner.js
@@ -1,9 +1,9 @@
 exports.groupowner = (prefix) => {
 return `*- LINK GROUP OWNER BOT*
 
-1. https://chat.whatsapp.com/G1PlSE3VR6e99hmMaxzpkg
-2. https://chat.whatsapp.com/LV3DOUhJerlHBkvAjcB5zr
-3. https://chat.whatsapp.com/LNCchYC5Iyp2VLpqy0oK7C
+1. https://chat.whatsapp.com/GE050K2kulEAOzNlqT6aqT
+2. https://chat.whatsapp.com/DD6faNVQDSgFMkd3l0Hzqs
+3. https://chat.whatsapp.com/BfpGLG0Zfdq0yGSWLbPFtT
 
-_Jika mau bergabung jangan lupa baca deskripsi!!_`
+*_Jangan Lupa Bergabung Bang_*`
 }
